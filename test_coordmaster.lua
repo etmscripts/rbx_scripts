3253d4c7f4a1a7ce86ae16a3408cad0095d25a52
dizyhvh <80992179+dizyhvh@users.noreply.github.com>
2022-01-06 04:32:12 +0300

    testing

---
+++ test_coordmaster.lua
@@ -0,0 +1,47 @@
+--[[
+    Coordmaster - undetected way of teleporting.
+    Bypasses most of games' anti-teleport.
+    
+    Soon I will add new feature to this script: Coordfly.
+]]
+
+local coordmaster = {};
+getgenv().coordmaster_tp_status = false;
+
+function coordmaster:Teleport(position, step_length, step_delay, callback)
+    if getgenv().coord_master_tp_status == true then return warn("[Coordmaster] Teleport is already working!") end
+    if typeof(position) ~= "CFrame" or typeof(position) ~= "Vector3" then return warn("[Coordmaster] Position is undefined."); end
+    if step_length == nil then return warn("[Coordmaster] Step length is nil/undefined."); end if step_delay == nil then return warn("[Coordmaster] Delay is nil/undefined."); end
+
+    if game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+        getgenv().coordmaster_tp_status = true;
+
+        local current_position = game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Position;
+        local steps = math.floor(math.sqrt((position.X - current_position.X) ^ 2 + (position.Y - current_position.Y) ^ 2 + (position.Z - current_position.Z) ^ 2 ) / step_length);
+        local path = {};
+
+        game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = true;
+
+        for i=1, steps do
+            path[#path+1] = {
+                x = current_position.X + ((position.X - current_position.X) / steps) * i,
+                y = current_position.Y + ((position.Y - current_position.Y) / steps) * i,
+                z = current_position.Z + ((position.Z - current_position.Z) / steps) * i,
+            }
+        end
+        path[#path+1] = {x = position.X, y = position.Y, z = position.Z};
+
+        for i=1, steps do
+            wait(step_delay);
+            game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(path[i].x, path[i].y, path[i].z);
+        end
+
+        game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Anchored = false;
+        wait(0.5);
+        callback();
+
+        getgenv().coordmaster_tp_status = false;
+    end
+end
+
+return coordmaster;
